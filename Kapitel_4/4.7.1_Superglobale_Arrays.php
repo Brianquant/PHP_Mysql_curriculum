a34135cfd7379c2441f12790e6b824d0f82af76d
Brianquant <reifferscheid.brianquant@gmail.com>
2021-08-02 13:49:48 +0200

    completed request,cookie & session

--- Kapitel_4/4.7.1_Superglobale_Arrays.php
+++ Kapitel_4/4.7.1_Superglobale_Arrays.php
@@ -7,7 +7,7 @@
 </head>
 <body>
 
-<!-- Array: POST START -->
+<!-- Array: $_POST START -->
 <form action="" method="POST">
     <input type="text" name="loginName" />
     <input type="password" name="loginPass" />
@@ -25,9 +25,9 @@ if(isset($loginName) || isset($loginPass)) {
 }
  ?>
 
- <!-- Array: POST END -->
+ <!-- Array: $_POST END -->
 
- <!-- Array: GET START -->
+ <!-- Array: $_GET START -->
 
  <form action="test_env.php" method="GET">
     <input type="text" name="loginName" />
@@ -49,7 +49,131 @@ if(isset($loginName) || isset($loginPass)) {
     header('Location: http://192.168.64.2/index.html');
 ?>
 
- <!-- Array: GET END -->
+<!-- Array: $_GET END -->
+
+<!-- Array: $_FILES START  -->
+<form action="test_env.php" method="post" enctype="multipart/form-data">
+  Select image to upload:
+  <input type="file" name="fileToUpload" id="fileToUpload">
+  <input type="submit" value="Upload Image" name="submit">
+</form>
+
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "File is an image - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "File is not an image.";
+    $uploadOk = 0;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 0;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+  $uploadOk = 0;
+}
+
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+  echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+  }
+}
+ ?>
+
+<!-- Array: $_FILES END  -->
+
+<!-- Array: $_REQUEST START -->
+    <form method="post" action="test_env.php">
+    Name: <input type="text" name="fname">
+    Phone: <input type="text" name="fnumber">
+    Adress: <input type="text" name="fadress">
+    Location: <input type="text" name="flocation">
+    <input type="submit">
+    </form>
+
+    <?php 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        $name = $_REQUEST['fname'];
+        $phone = $_REQUEST['fnumber'];
+        $adress = $_REQUEST['fadress'];
+        $location = $_REQUEST['flocation'];
+        if (empty($name)) {
+          echo "Name is empty";
+        } else {
+          echo $name;
+          echo "<br/>";
+          echo $phone;
+          echo "<br/>";
+          echo $adress;
+          echo "<br/>";
+          echo $location;
+        }
+      }
+    ?>
+<!-- Array: $_REQUEST END -->
+
+<!-- Array: $_COOKIE START -->
+<?php
+$content = 'Fischers Fritz fischt frische Fische';
+setcookie('Tonguebreaker', $content, time()+86400);
+
+echo $_COOKIE['Tonguebreaker'];
+ ?>
+<!-- Array: $_COOKIE END -->
+
+
+<!-- Array: $_SESSION START -->
+<form method="post" action="test_env.php">
+Name: <input type="text" name="fname">
+Phone: <input type="text" name="fnumber">
+<input type="submit">
+</form>
+<?php
+$name = $_POST["fname"];
+$phone = $_POST["fnumber"];
+
+$_SESSION["name"] = $name;
+$_SESSION["number"] = $phone;
+echo "Session variables are set.";
+
+echo "<br/>";
+echo $_SESSION["name"];
+echo "<br/>";
+echo $_SESSION["number"];
+print_r($_SESSION);
+ ?>
+<!-- Array: $_SESSION END -->
+
+
  
 </body>
 </html>
