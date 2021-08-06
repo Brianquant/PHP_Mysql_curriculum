bd5a9eb07555507dee6dd900c28256d68c594923
Brianquant <reifferscheid.brianquant@gmail.com>
2021-08-06 12:54:34 +0200

    completed 5.2.3

--- Kapitel_5/5.2.2_Zugriffsmodifizierer.php
+++ Kapitel_5/5.2.2_Zugriffsmodifizierer.php
@@ -1,5 +1,50 @@
 <?php 
 
+/**
+ * public(+) Element is von jedem Scope erreichbar
+ * protected(#) Element is nur von Objekten der eigenen Klasse erreichbar oder von abgeleiteten Klassen
+ * private(-) Element is nur von von Objekten der eigenen Klasse erreichbar
+ */
+
+
+class Fahrzeug {
+    public $gestartet = "Aus";
+    public $aktuelleGeschwindigkeit = 0;
+    public $hoechstGeschwindigkeit = 180;
+  
+    function status() {
+       $this->gestartet;
+       $this->aktuelleGeschwindigkeit;
+       $this->hoechstGeschwindigkeit;
+    }
+    protected function greifen() {
+        echo "Ich greife";
+        echo "<br />";
+      }
+    
+  }
+  
+  $obj = new Fahrzeug();
+  echo $obj->gestartet; // Funktioniert
+  echo "<br />";
+  echo $obj->aktuelleGeschwindigkeit; // Fataler Fehler
+  echo "<br />";
+  echo $obj->hoechstGeschwindigkeit; // Fataler Fehler
+  echo "<br />";
+  $obj->status();
+
+  // Gültigkeitsbereichsoperator
+class Bremse extends Fahrzeug {
+
+    public function greifen() {
+      parent::greifen();
+      echo "Ich greife";
+    }
+  
+  }
+  
+  $class = new Bremse();
+  $class->greifen();
 
 
 
