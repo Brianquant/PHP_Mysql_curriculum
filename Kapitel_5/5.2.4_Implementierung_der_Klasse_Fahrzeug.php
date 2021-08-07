79e28badc8dd84050cd642db1c9955a20a76748e
Brianquant <reifferscheid.brianquant@gmail.com>
2021-08-07 17:49:42 +0200

    completed 5.2.4

---
+++ Kapitel_5/5.2.4_Implementierung_der_Klasse_Fahrzeug.php
@@ -0,0 +1,42 @@
+<?php 
+
+
+class Fahrzeug
+{
+  // Attribute
+  public $gestartet = false;
+  protected $aktuelleGeschwindigkeit = 0;
+  private $hoechstGeschwindigkeit = 0;
+
+  // Konstruktor
+  function __construct($maxV = 180)
+  {
+    $this->hoechstGeschwindigkeit = $maxV;
+  }
+  public function starteMotor() {
+    $this->gestartet = true;
+  }
+  public function stoppeMotor() {
+    $this->gestartet = false;
+  }
+  public function beschleunigen($neuV) {
+    if($this->gestartet) {
+      if($neuV <= $this->hoechstGeschwindigkeit) {
+        $this->aktuelleGeschwindigkeit = $neuV;
+      } else {
+        $this->aktuelleGeschwindigkeit = $this->hoechstGeschwindigkeit;
+      }
+    }
+  }
+
+  public function bremse($neuV) {
+    if($neuV <= $this->aktuelleGeschwindigkeit){
+      $this->aktuelleGeschwindigkeit = $neuV;
+    } else {
+      $this->aktuelleGeschwindigkeit = $this->aktuelleGeschwindigkeit;
+    }
+  }
+  
+}
+
+?>
