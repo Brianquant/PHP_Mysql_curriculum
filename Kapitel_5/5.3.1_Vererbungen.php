e5087c122514d8f080d153da0025645612e1510c
Brianquant <reifferscheid.brianquant@gmail.com>
2021-08-12 21:46:40 +0200

    chapter 5.3.1

--- Kapitel_5/5.3.1_Vererbungen.php
+++ Kapitel_5/5.3.1_Vererbungen.php
@@ -11,5 +11,17 @@ class LKW extends Fahrzeug {
   $lkw->starteMotor();
   echo($lkw->gestartet)?'an':'aus';
   $lkw->beschleunigen(100);
+class LKW2 extends Fahrzeug {
+    function __construct($zuladung,$maxV = 100)
+    {
+      $this->zuladung = $zuladung;
+      parent::__construct($maxV);
+    }
+  }
+  
+  $lkw = new LKW(500,75);
+  $lkw->starteMotor();
+  echo($lkw->gestartet)?'an':'aus';
+  $lkw->beschleunigen(100);
 
 ?>
