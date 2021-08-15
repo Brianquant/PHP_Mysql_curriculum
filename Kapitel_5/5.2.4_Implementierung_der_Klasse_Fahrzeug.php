49b4368223f5a2960968d09ad5c80cb8c26fd940
Brianquant <reifferscheid.brianquant@gmail.com>
2021-08-15 22:20:31 +0200

    refactor class Fahrzeug

--- Kapitel_5/5.2.4_Implementierung_der_Klasse_Fahrzeug.php
+++ Kapitel_5/5.2.4_Implementierung_der_Klasse_Fahrzeug.php
@@ -37,7 +37,53 @@ class Fahrzeug
       $this->aktuelleGeschwindigkeit = $this->aktuelleGeschwindigkeit;
     }
   }
-  
 }
+
+// Refactor
+class Fahrzeug2 {
+  public $gestartet;
+  protected $aktuelleGeschwindigkeit;
+  protected $hoechstGeschwindigkeit;
+
+  function __construct($istGestartet, $istAktuelleGeschwindigkeit, $istHoechstGeschwindigkeit)
+  {
+    $this->gestartet = $istGestartet;
+    $this->aktuelleGeschwindigkeit = $istAktuelleGeschwindigkeit;
+    $this->hoechstGeschwindigkeit = $istHoechstGeschwindigkeit;
+  }
+  public function starteMotor() {
+    $this->gestartet = 'true';
+  }
+  public function stoppeMotor() {
+    $this->gestartet = 'false';
+  }
+
+  public function beschleunigen($neuV) {
+    if($this->gestartet) {
+      if($neuV <= $this->hoechstGeschwindigkeit) {
+        echo $this->aktuelleGeschwindigkeit = $neuV;
+      }
+      else 
+      {
+        echo $this->aktuelleGeschwindigkeit = $this->hoechstGeschwindigkeit;
+      }
+    }
+  }
+
+  public function bremse($neuV) {
+    if($neuV <= $this->aktuelleGeschwindigkeit) {
+      $this->aktuelleGeschwindigkeit = $neuV;
+    } 
+    else 
+    {
+      $this->aktuelleGeschwindigkeit = $this->aktuelleGeschwindigkeit;
+    }
+  }
+
+
+}
+
+$fahrzeug1 = new Fahrzeug2('true' ,0 ,180);
+$fahrzeug1->beschleunigen(0);
 
 ?>
